e4b904d32607f6a23b1f958b000dfc72993c4c5b
Mirunalikha <230901062@rajalakshmi.edu.in>
2025-02-03 14:23:08 +0530

    Add files via upload

---
+++ mirunalikhanumpy2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+arr=np.array([1,2,3,4,5,6,7,8,9])
+slice1=arr[2:6]
+print(slice1)
+
+
+# In[2]:
+
+
+slice2=arr[::2]
+print(slice2)
+
+
+# In[3]:
+
+
+import numpy as np
+arr=([1,2,3,4,5,6,7,8,9])
+reversed_arr=arr[::-1]
+print(reversed_arr)
+
+
+# In[4]:
+
+
+slice3=arr[::3]
+print(slice3)
+
+
+# In[6]:
+
+
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6],[7,8,9]])
+subarray=arr[0:2,1:3]
+print(subarray)
+
+
+# In[ ]:
+
+
+
+
